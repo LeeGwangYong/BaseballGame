012049635e17abae8397a7b1d7a500cd844fbd53
이광용 <rhkdrmfh@gmail.com>
2019-12-03 22:34:41 +0900

    GameProcessor를 구현하여라
    HitterState에 따라 알맞은 GameProgress를 반환한다.
    Ball이 `maximumBall`보다 크거나 같을 경우, 안타로 취급한다.
    Strike가 `maximumStrike`보다 크거나 같을 경우, Out으로 취급하며,
    Out이 `maximumOut`보다 크거나 같을 경우, 게임을 종료한다.

--- BaseballGame/Core/GameProcessor.swift
+++ BaseballGame/Core/GameProcessor.swift
@@ -8,6 +8,50 @@
 
 import Foundation
 
-func process() {
-    
+func process(hitterState: HitterState, progress: GameProgress) -> GameProgress {
+    switch hitterState {
+    case .hit:
+        output(types: [.hit, .nextHitter])
+        return hitted(progress)
+    case .ball:
+        output(.ball)
+        return balled(progress)
+    case .strike:
+        output(.strike)
+        return striked(progress)
+    }
+}
+
+private func hitted(_ progress: GameProgress) -> GameProgress {
+    return GameProgress(hitter: Hitter(),
+                        scoreBoard: ScoreBoard(hit: progress.scoreBoard.hit + 1,
+                                               out: progress.scoreBoard.out),
+                        isOvered: progress.isOvered)
+}
+
+private let maximumBall = 4
+private func balled(_ progress: GameProgress) -> GameProgress {
+    let hitter = Hitter(strike: progress.hitter.strike,
+                        ball: progress.hitter.ball + 1)
+    return hitter.ball >= maximumBall ?
+        process(hitterState: .hit, progress: progress) : GameProgress(hitter: hitter, scoreBoard: progress.scoreBoard, isOvered: progress.isOvered)
+}
+
+private let maximumStrike = 3
+private let maximumOut = 3
+private func striked(_ progress: GameProgress) -> GameProgress {
+    let hitter = Hitter(strike: progress.hitter.strike + 1,
+                        ball: progress.hitter.ball)
+    if hitter.strike >= maximumStrike {
+        let scoreBoard = ScoreBoard(hit: progress.scoreBoard.hit, out: progress.scoreBoard.out + 1)
+        let isGameOvered = scoreBoard.out >= maximumOut
+        isGameOvered ? output(.out) : output(types: [.out, .nextHitter])
+        return GameProgress(hitter: Hitter(),
+                            scoreBoard: scoreBoard,
+                            isOvered: isGameOvered)
+    } else {
+        return GameProgress(hitter: hitter,
+                            scoreBoard: progress.scoreBoard,
+                            isOvered: progress.isOvered)
+    }
 }
