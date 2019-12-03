4b805a7a69cded6b6d5df98218ee3c0a61a86159
이광용 <rhkdrmfh@gmail.com>
2019-12-03 21:21:53 +0900

    최상위 추상적인 함수를 구현해라
    Runner를 이용하여 Input-Processor-Output의 흐름 순으로 게임을 구동한다.

---
+++ BaseballGame/Core/GameInput.swift
@@ -0,0 +1,13 @@
+//
+//  GameInput.swift
+//  BaseballGame
+//
+//  Created by 이광용 on 2019/12/03.
+//  Copyright © 2019 GwangYongLee. All rights reserved.
+//
+
+import Foundation
+
+func input() {
+    
+}
