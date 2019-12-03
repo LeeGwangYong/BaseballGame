9e3996449766327f10a7eb9f8226d256e95d7330
이광용 <rhkdrmfh@gmail.com>
2019-12-03 21:50:11 +0900

    HitterState를 추가하라
    Input에서 사용하기 위하여 추가함

---
+++ BaseballGame/Core/Model/HitterState.swift
@@ -0,0 +1,17 @@
+//
+//  HitterState.swift
+//  BaseballGame
+//
+//  Created by 이광용 on 2019/12/03.
+//  Copyright © 2019 GwangYongLee. All rights reserved.
+//
+
+import Foundation
+
+enum HitterState: CaseIterable {
+
+    case strike
+    case ball
+    case hit
+    
+}
