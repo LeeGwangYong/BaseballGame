629e2433e3f7a7b3789928d79887e6beb0d09abd
이광용 <rhkdrmfh@gmail.com>
2019-12-04 16:51:28 +0900

    `run`을 내부적으로 사용하고, `gameStart`로 초기 진입을 변경하여라
    `gameStart`라는 것이 초기 진입에 대한 명명에 더 올바르다고 보여 변경하였다.
    메뉴에 대한 표기와 메뉴 선택, 게임 구동이 가능하도록 `gameStart`에서 결정하는 책임을 맡는다.
    `run`은 이전과 유사하게 게임의 구동에 대한 책임을 맡는다.

--- BaseballGame/Core/GameRunner.swift
+++ BaseballGame/Core/GameRunner.swift
@@ -2,31 +2,80 @@
 //  GameRunner.swift
 //  BaseballGame
 //
-//  Created by 이광용 on 2019/12/03.
+//  Created by 이광용 on 2019/12/04.
 //  Copyright © 2019 GwangYongLee. All rights reserved.
 //
 
 import Foundation
 
-func run() {
-    let hitter = Hitter()
-    let scoreBoard = ScoreBoard()
-    let gameProgress = GameProgress(hitter: hitter, scoreBoard: scoreBoard, isOvered: false)
-    output(.gameStart)
-    output(.firstHitter)
-    output(.empty)
-    recursivelyRun(gameProgress)
+enum GameRunnerLog: CustomStringConvertible {
+
+    case opening
+    case menus(_ menus: [Menu])
+    case versus(_ topTeam: Team, _ bottomTeam: Team)
+    case start(_ topTeam: Team, _ bottomTeam: Team)
+    case finalScore(_ topProgress: GameProgress, _ bottomProgress: GameProgress)
+    case end
+    case closing
+
+
+    var description: String {
+        switch self {
+        case .opening:
+            return "신나는 야구시합"
+        case .menus(let menus):
+            return menus.map { MenuOutputLog.menu($0).description }.joined(separator: .newLine) + .newLine
+        case .versus(let topTeam, let bottomTeam):
+            return "\(topTeam.name) VS \(bottomTeam.name)"
+        case .start(let topTeam, let bottomTeam):
+            return GameRunnerLog.versus(topTeam, bottomTeam).description + "의 시합을 시작합니다." + .newLine
+        case .finalScore(let topProgress, let bottomProgress):
+            return "\(topProgress.score) : \(bottomProgress.score)"
+        case .end:
+            return "경기 종료"
+        case .closing:
+            return "Thank you!"
+        }
+    }
+
 }
 
-private func recursivelyRun(hitterState: HitterState = hitterState(),
-                            _ progress: GameProgress) {
-    guard !progress.isOvered else {
-        output(.finalScore(scoreboard: progress.scoreBoard))
-        output(.gameOver)
-        return
+func gameStart() throws {
+    var mutableTeams = [Team]() 
+
+    while(true) {
+        print(GameRunnerLog.opening)
+        print(GameRunnerLog.menus(Menu.allCases))
+        let menu = try inputMenu()
+        switch menu {
+        case .inputData:
+            mutableTeams = try inputTeams()
+        case .outputData:
+            try output(teams: mutableTeams)
+        case .run:
+            try run(teams: mutableTeams)
+            return
+        }
     }
-    let processedProgress = process(hitterState: hitterState, progress: progress)
-    output(.currentProgress(progress: processedProgress))
-    output(.empty)
-    recursivelyRun(processedProgress)
+}
+
+private func run(teams: [Team]) throws {
+    guard let topTeam = teams[safe: 0],
+        let bottomTeam = teams[safe: 1] else {
+            throw TeamOutputError.teamIsWrongOrNotExsist
+    }
+    print(GameRunnerLog.start(topTeam, bottomTeam))
+    let result = try process(topTeam: topTeam, topProgress: GameProgress(memberIndex: 0),
+                             bottomTeam: bottomTeam, bottomProgress: GameProgress(memberIndex: 0))
+    log(topTeam: topTeam, topProgress: result.0,
+        bottomTeam: bottomTeam, bottomProgress: result.1)
+}
+
+private func log(topTeam: Team, topProgress: GameProgress,
+                 bottomTeam: Team, bottomProgress: GameProgress) {
+    print(GameRunnerLog.end)
+    print()
+    print(GameRunnerLog.versus(topTeam, bottomTeam))
+    print(GameRunnerLog.finalScore(topProgress, bottomProgress))
+    print(GameRunnerLog.closing)
 }
