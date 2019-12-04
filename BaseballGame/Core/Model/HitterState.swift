29bf7866ca395dbf47a18e9908e5aea7e571c427
이광용 <rhkdrmfh@gmail.com>
2019-12-04 16:46:04 +0900

    HitterState가 CustomStringConvertible을 따르게 하라

--- BaseballGame/Core/Model/HitterState.swift
+++ BaseballGame/Core/Model/HitterState.swift
@@ -8,11 +8,24 @@
 
 import Foundation
 
-enum HitterState: Int, CaseIterable {
+enum HitterState: Int, CaseIterable, CustomStringConvertible {
 
     case hit
     case strike
     case ball
     case out
+
+    var description: String {
+        switch self {
+        case .hit:
+            return "안타!"
+        case .strike:
+            return "스트라이크!"
+        case .ball:
+            return "볼!"
+        case .out:
+            return "아웃!"
+        }
+    }
     
 }
