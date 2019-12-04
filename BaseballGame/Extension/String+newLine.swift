12dc18c273b62a4659e90d9af8a28973d2a9d4ac
이광용 <rhkdrmfh@gmail.com>
2019-12-04 16:46:54 +0900

    Add String+newLine

---
+++ BaseballGame/Extension/String+newLine.swift
@@ -0,0 +1,17 @@
+//
+//  String+newLine.swift
+//  BaseballGame
+//
+//  Created by 이광용 on 2019/12/04.
+//  Copyright © 2019 GwangYongLee. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    static var newLine: String {
+        return "\n"
+    }
+
+}
