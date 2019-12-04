595c380dc10d5bacf2e5a44a2ebedc058c5446f5
이광용 <rhkdrmfh@gmail.com>
2019-12-04 15:28:42 +0900

    `hitterSttate(of:)`의 책임을 분리하여라
    `averages(of:)`에 Member의 타율에 따른 스트라이크, 볼의 확률을 도출하는 책임을 주었다.

--- BaseballGame/Core/Input/HitterStateOfMember.swift
+++ BaseballGame/Core/Input/HitterStateOfMember.swift
@@ -10,12 +10,19 @@ import Foundation
 
 func hitterState(of member: Member) throws -> HitterState {
     let hitAverage = member.hitAverage
-    let strikeAverage = (1 - hitAverage) / 2 - 0.05
-    let ballAverage = (1 - hitAverage) / 2 - 0.05
     let outAverage = 0.1
-    let rawValue = [hitAverage, strikeAverage, ballAverage, outAverage].randomNumberWithGivenDistribution()
-    guard let state = HitterState(rawValue: rawValue) else {
+    let averages = [hitAverage, strikeAverage(from: hitAverage), ballAverage(from: hitAverage), outAverage]
+    let rawValueOfHitterState = averages.randomNumberWithGivenDistribution()
+    guard let state = HitterState(rawValue: rawValueOfHitterState) else {
         throw HitterStateError.isNoMatchingHitterState
     }
     return state
+}
+
+private func strikeAverage(from hitAverage: Double) -> Double {
+    return (1 - hitAverage) / 2 - 0.05
+}
+
+private func ballAverage(from hitAverage: Double) -> Double  {
+    return (1 - hitAverage) / 2 - 0.05
 }
