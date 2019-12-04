94a7cb74c0a1b9261eebb317d983d6bfe8f75762
이광용 <rhkdrmfh@gmail.com>
2019-12-04 16:49:01 +0900

    Processor를 업데이트하여라
    게임 진행에 있어 라운드, 초 / 말, 팀, 맴버(타자)에 따라 모두 처리가능하도록 구현하였다

--- BaseballGame/Core/GameProcessor.swift
+++ BaseballGame/Core/GameProcessor.swift
@@ -8,25 +8,107 @@
 
 import Foundation
 
-func process(hitterState: HitterState, progress: GameProgress) -> GameProgress {
-    switch hitterState {
+enum ProcessLog: CustomStringConvertible {
+
+    case attack(round: Int, order: AttackOrder, team: Team)
+    case member(_ mebmer: Member)
+    case hitterState(_ state: HitterState)
+    case currentProgress(_ progress: GameProgress)
+
+    var description: String {
+        switch self {
+        case .attack(let round, let order, let team):
+            return "\(round)회\(order.description) \(team.name)의 공격" + .newLine
+        case .member(let member):
+            return "\(member.number)번 \(member.name)"
+        case .hitterState(let state):
+            return state.description
+        case .currentProgress(let progress):
+            return "\(progress.hitter.strike)S, \(progress.hitter.ball)B, \(progress.scoreBoard.out)O"
+        }
+    }
+
+}
+
+func process(about round: Int = 1,
+             topTeam: Team, topProgress: GameProgress,
+             bottomTeam: Team, bottomProgress: GameProgress) throws -> (GameProgress, GameProgress)  {
+        let maximumRound = 6
+        guard round <= maximumRound else {
+            return (topProgress, bottomProgress)
+        }
+        let progresses = try AttackOrder.allCases.map { order -> GameProgress in
+            let team = order == .top ? topTeam : bottomTeam
+            let previousProgrss = order == .top ? topProgress : bottomProgress
+            print(ProcessLog.attack(round: round, order: order, team: team))
+            return try process(about: team, progress: GameProgress(score: previousProgrss.score,
+                                                                   memberIndex: previousProgrss.memberIndex))
+        }
+        return try process(about: round + 1,
+                           topTeam: topTeam, topProgress: progresses[0],
+                           bottomTeam: bottomTeam, bottomProgress: progresses[1])
+}
+
+private func process(about team: Team, progress: GameProgress) throws -> GameProgress {
+    guard !progress.isOvered else {
+        return progress
+    }
+    let member =  team.members[progress.memberIndex]
+    print(ProcessLog.member(member))
+    return try process(about: team, progress: process(about: member, progress: progress))
+}
+
+private func process(about member: Member, progress: GameProgress) throws -> GameProgress {
+    let state = try hitterState(of: member)
+    switch state {
     case .hit:
-        output(types: [.hit, .nextHitter])
-        return hitted(progress)
+        return process(hitterState: state, progress: progress)
     case .ball:
-        output(.ball)
-        return balled(progress)
+        let _progress = process(hitterState: state, progress: progress)
+        return try process(about: member, progress: _progress)
     case .strike:
-        output(.strike)
-        return striked(progress)
+        guard progress.hitter.strike + 1 < maximumStrike else {
+            print(ProcessLog.hitterState(.strike))
+            return process(hitterState: .out, progress: progress)
+        }
+        let _progress = process(hitterState: state, progress: progress)
+        return try process(about: member, progress: _progress)
+    case .out:
+        return process(hitterState: state, progress: progress)
     }
 }
 
+private func process(hitterState: HitterState, progress: GameProgress) -> GameProgress {
+    print(ProcessLog.hitterState(hitterState))
+    let _progress: GameProgress
+    switch hitterState {
+    case .hit:
+        _progress = hitted(progress)
+    case .ball:
+        _progress = balled(progress)
+    case .strike:
+        _progress = striked(progress)
+    case .out:
+        _progress = outed(progress)
+    }
+    print(ProcessLog.currentProgress(_progress))
+    print()
+    return _progress
+}
+
 private func hitted(_ progress: GameProgress) -> GameProgress {
+    let scoreBoard = ScoreBoard(hit: progress.scoreBoard.hit + 1,
+                                out: progress.scoreBoard.out)
     return GameProgress(hitter: Hitter(),
-                        scoreBoard: ScoreBoard(hit: progress.scoreBoard.hit + 1,
-                                               out: progress.scoreBoard.out),
+                        scoreBoard: scoreBoard,
+                        score: score(by: scoreBoard.hit),
+                        memberIndex: progress.memberIndex + 1,
                         isOvered: progress.isOvered)
+}
+
+private func score(by hit: Int) -> Int {
+    let _score = hit - 3
+    return _score > 0 ? _score : 0
 }
 
 private let maximumBall = 4
@@ -34,7 +116,11 @@ private func balled(_ progress: GameProgress) -> GameProgress {
     let hitter = Hitter(strike: progress.hitter.strike,
                         ball: progress.hitter.ball + 1)
     return hitter.ball >= maximumBall ?
-        process(hitterState: .hit, progress: progress) : GameProgress(hitter: hitter, scoreBoard: progress.scoreBoard, isOvered: progress.isOvered)
+        process(hitterState: .hit, progress: progress) : GameProgress(hitter: hitter,
+                                                                      scoreBoard: progress.scoreBoard,
+                                                                      score: progress.score,
+                                                                      memberIndex: progress.memberIndex,
+                                                                      isOvered: progress.isOvered)
 }
 
 private let maximumStrike = 3
@@ -42,16 +128,19 @@ private let maximumOut = 3
 private func striked(_ progress: GameProgress) -> GameProgress {
     let hitter = Hitter(strike: progress.hitter.strike + 1,
                         ball: progress.hitter.ball)
-    if hitter.strike >= maximumStrike {
-        let scoreBoard = ScoreBoard(hit: progress.scoreBoard.hit, out: progress.scoreBoard.out + 1)
-        let isGameOvered = scoreBoard.out >= maximumOut
-        isGameOvered ? output(.out) : output(types: [.out, .nextHitter])
-        return GameProgress(hitter: Hitter(),
-                            scoreBoard: scoreBoard,
-                            isOvered: isGameOvered)
-    } else {
-        return GameProgress(hitter: hitter,
-                            scoreBoard: progress.scoreBoard,
-                            isOvered: progress.isOvered)
-    }
+    return GameProgress(hitter: hitter,
+                        scoreBoard: progress.scoreBoard,
+                        score: progress.score,
+                        memberIndex: progress.memberIndex,
+                        isOvered: progress.isOvered)
+}
+
+private func outed(_ progress: GameProgress) -> GameProgress {
+    let scoreBoard = ScoreBoard(hit: progress.scoreBoard.hit, out: progress.scoreBoard.out + 1)
+    let isGameOvered = scoreBoard.out >= maximumOut
+    return GameProgress(hitter: Hitter(),
+                        scoreBoard: scoreBoard,
+                        score: progress.score,
+                        memberIndex: progress.memberIndex + 1,
+                        isOvered: isGameOvered)
 }
