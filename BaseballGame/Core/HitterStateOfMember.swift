80d49c1931fae64982308bc407921ae3afe8a404
이광용 <rhkdrmfh@gmail.com>
2019-12-04 16:45:12 +0900

    Input을 지우고 HitterStateOfMember로 변경하라

--- BaseballGame/Core/HitterStateOfMember.swift
+++ BaseballGame/Core/HitterStateOfMember.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+enum HitterStateError: LocalizedError {
+
+    case isNoMatchingHitterState
+
+    var errorDescription: String? {
+        switch self {
+        case .isNoMatchingHitterState:
+            return "알맞는 HitterState가 존재하지 않습니다."
+        }
+    }
+
+}
+
 func hitterState(of member: Member) throws -> HitterState {
     let rawValueOfHitterState = averages(of: member).randomNumberWithGivenDistribution()
     guard let state = HitterState(rawValue: rawValueOfHitterState) else {
